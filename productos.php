d5cec079c847150a988a0c8dfa9fced161bdd521
jonathanR2021 <jonathan.ramos20@itca.edu.sv>
2021-06-04 13:14:49 -0600

    Agregue el php para trabajar los productos

---
+++ productos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="">
+<table border="0px" >
+         
+         <tbody>
+           <tr>
+             <td for="username"> Nombre
+               <input type="text" placeholder="nombre de articulo"></td>          
+           </tr>
+           <tr>
+              
+             <td for="username"> Detalle
+              <input type="text" placeholder="Detalle del articulo"></td>
+             
+            
+
+           </tr>
+           <tr>
+              <!-- DIRECCIÓN-->
+             <td for="username">  Precio
+             <input type="text" placeholder="Ingrese precio">
+              </td>
+            
+           </tr>
+        <td>
+        <input type="submit" value="Registrar">
+        <input type="submit" value="Buscar">
+       
+         <br>
+         <input type="submit" value="Eliminar registro">
+         <input type="reset" value="Borrar">
+
+        </td>
+        
+         </tbody>
+         </table>
+         </form>
+</body>
+</html>
