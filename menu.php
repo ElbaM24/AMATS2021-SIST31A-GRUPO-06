a3bd9cd6adf12e398d9031f79f47d11bbd382c60
jonathanR2021 <jonathan.ramos20@itca.edu.sv>
2021-06-04 12:59:12 -0600

    Agregue el archivo menu.php

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<table>
+    <tbody>
+      <tr>
+        <td colspan="2" align="center">
+          <h1 align="center">CATEGORIAS </h1>
+          <input type="submit" value="Pupusas">
+          <input type="submit" value="Bebidas">
+          <br>
+          <input type="submit" value="Combos">
+          <input type="submit" value="Desayunos">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
