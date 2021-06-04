d0840fd20d9585a69f2c022d9b2db3a1103c6614
jonathanR2021 <jonathan.ramos20@itca.edu.sv>
2021-06-04 12:52:27 -0600

    Agregue el archivo cliente.php

---
+++ cliente.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <table border="0px" class="tabla1">
+
+            <tbody>
+                <tr>
+                    <!-- DUI -->
+                    <td for="username"> Nombres del cliente
+                        <input type="text" placeholder="nombre">
+                    </td>
+                </tr>
+                <tr>
+
+                    <td for="username"> Apellidos del cliente
+                        <input type="text" placeholder="Apellidos">
+                    </td>
+                </tr>
+                <tr>
+                    <!-- DIRECCIÓN-->
+                    <td for="username"> Dirección del cliente
+                        <input type="text" placeholder="Dirección">
+                    </td>
+                </tr>
+                <td>
+                    <input type="submit" value="Registrar">
+                    <input type="submit" value="Buscar">
+                    <br>
+                    <input type="submit" value="Eliminar registro">
+                    <input type="reset" value="Borrar">
+
+                </td>
+
+            </tbody>
+        </table>
+    </form>
+</body>
+
+</html>
