ecfb22daabf98d4c4b502bb43495aba7fe5b8c7a
jonathanR2021 <jonathan.ramos20@itca.edu.sv>
2021-06-07 10:29:16 -0600

    Agregue productos.php a menu de admin

--- admin.php
+++ admin.php
@@ -15,6 +15,7 @@
         <ul>
             <li><a href="admin.php?pagina=cargo.php">Cargo</a></li>
             <li><a href="registro.php">Registro</a></li><!--agregue esto admin.php?-->
+            <li><a href="admin.php?pagina=productos.php">Productos</a></li>
             <li><a href="admin.php?pagina=pedidos.php">Pedidos</a></li>
             <li><a href="admin.php?pagina=cliente.php">Clientes</a></li>
             <li><a href="admin.php?pagina=reportes.php">Reportes</a></li>
